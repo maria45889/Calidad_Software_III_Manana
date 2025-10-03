2d0cd46c311c37d51d6bd9547b5d38e4870a6f1f
maria <mariajosetacoc2005@gmail.com>
2025-10-03 09:21:41 -0500

    Practicas de git en calidad de software

--- modulo_1/myapp.py
+++ modulo_1/myapp.py
@@ -2,3 +2,6 @@ print("PRUEBA DESDE CALIDAD DEL SOFTWARE")
 materia = "Calidad del Software"
 if (materia):
     print("Estoty praticando git en: ", materia)
+    
+if (materia):
+    print("No existe materia")
